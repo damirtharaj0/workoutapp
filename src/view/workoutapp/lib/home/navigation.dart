0be01212c3da2a1f0756ef924d6882085c72e911
dannyaam9 <dannyaam9@gmail.com>
2021-12-31 17:19:01 -0800

    updated navigation page

--- src/view/workoutapp/lib/home/navigation.dart
+++ src/view/workoutapp/lib/home/navigation.dart
@@ -5,20 +5,15 @@ import '../screens/plans/plan_page.dart';
 import '../screens/profile/profile.dart';
 
 class Navigation extends StatefulWidget {
-  const Navigation({ Key? key }) : super(key: key);
+  const Navigation({Key? key}) : super(key: key);
 
   @override
   _NavigationState createState() => _NavigationState();
 }
 
 class _NavigationState extends State<Navigation> {
-  
   int _selectedIndex = 0;
-  List labels = [
-    'Plans',
-    'Current Workout',
-    'Profile'
-  ];
+  List labels = ['Plans', 'Current Workout', 'Profile'];
   List<Widget> _widgetOptions = <Widget>[
     PlansPage(),
     CurrentWorkout(),
@@ -32,27 +27,16 @@ class _NavigationState extends State<Navigation> {
         centerTitle: true,
         title: Text(
           labels[_selectedIndex],
-          style: TextStyle(
-            fontSize: 25,
-            fontWeight: FontWeight.bold 
-          ),
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
         ),
       ),
       body: _widgetOptions[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         items: [
+          BottomNavigationBarItem(icon: Icon(Icons.next_plan), label: 'Plans'),
           BottomNavigationBarItem(
-            icon: Icon(Icons.next_plan),
-            label: 'Plans'
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.directions_run),
-            label: 'Current Workout'
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Plans'
-          )
+              icon: Icon(Icons.directions_run), label: 'Current Workout'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Plans')
         ],
         currentIndex: _selectedIndex,
         onTap: (int index) {
