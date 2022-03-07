71aae0f1c0edf0f9a1bed8eb299a074aeade3054
dannyaam9 <dannyaam9@gmail.com>
2022-03-07 14:31:41 -0800

    get plans and add plan

--- src/controller/db.py
+++ src/controller/db.py
@@ -6,17 +6,18 @@ class Database:
         cluster = MongoClient(
             "mongodb+srv://sauceque:Cricket123@cluster0.rahww.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
         db = cluster['WorkoutApp']
-        self.collection = db['users']
+        self.users = db['users']
+        self.plans = db['plans']
 
     def addUser(self, username, userEmail):
-        if self.collection.find_one(username) == None:
+        if self.users.find_one(username) == None:
             return {'success' : False, 'message' : 'user already exists'}, 400
         data = {
             username: {
                 "userEmail": userEmail,
             }
         }
-        self.collection.update_one(
+        self.users.update_one(
             {},
             {
                 "$set" : data
@@ -24,8 +25,42 @@ class Database:
             upsert=False,
             array_filters=None
         )
-        if self.collection.find_one(data) == None:
+        if self.users.find_one(data) == None:
             return {'success' : False, 'message' : 'could not add user'}, 400
         
         return {'success' : True, 'message' : 'successfully added user'}, 200
+
+    def getPlans(self):
+        plans = self.plans.find()
+        planslst = []
+        plan = plans[0]
+        print
+        for i in range(1, len(plan)):
+            print(i)
+            plntpl = list(plan.items())[i]
+            planDict = plntpl[1]
+            planDict["planName"] = plntpl[0]
+            planslst.append(planDict)
+        return {'success' : True, 'data' : planslst}, 200
+
+    def addPlan(self, planName, exerciseNames, reps, weight):
+        data = {
+            planName : {
+                "exerciseNames" : exerciseNames,
+                "reps" : reps,
+                "weight" : weight,
+                "totalVolume" : 10
+            }
+        }
+        self.plans.update_one(
+            {},
+            {
+                "$set" : data
+            },
+            upsert=False,
+            array_filters=None
+        )
+        if self.plans.find_one(data) == None:
+            return {'success' : False, 'message' : 'could not add plan'}, 400
+        return {'success' : True, 'message' : 'successfully added plan'}, 200
         
